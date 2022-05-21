099888a37adaddae039813cea65282f3c27a7110
Faizan <puncturelgao@gmail.com>
2022-05-21 17:17:01 +0500

    from one activity to another activity and view

---
+++ activity1_to_2/app/src/main/java/com/example/activity1_to_2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.activity1_to_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b;
+        b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,MainActivity2.class);
+                String s ="My Name is Zainab";
+                intent.putExtra("a",s);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ activity1_to_2/app/src/main/java/com/example/activity1_to_2/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.activity1_to_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t=findViewById(R.id.textView);
+        Intent intent=getIntent();
+        String b=intent.getStringExtra("a");
+        t.setText(b);
+
+
+
+    }
+}
